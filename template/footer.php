53836e659b751c2f0de2124d4e3a77656e359ffd
hartpableo <hart.hpwebdev@gmail.com>
2026-07-01 07:13:09 +0800

    refactor: added source code link to footer

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,8 @@
 <footer role="contentinfo" class="footer mt-auto py-4 bg-white border-top">
   <div class="container text-center">
     <span class="text-muted">Developed with ❤️ by <a href="https://hartpableo.com" target="_blank" class="text-decoration-none fw-semibold text-gradient">Hart</a></span>
+    <br>
+    <span class="text-muted">View source code <a href="https://github.com/hartpableo/php-rest-api" target="_blank">here</a>.</span>
   </div>
 </footer>
 </body>
